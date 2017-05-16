43f67c4f23fdd4137a9b766c5e20b3ade5499a46
jtknox91 <jtknox91@gmail.com>
2017-05-15 22:08:10 -0700

    Update directory and log message

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express()
 
 // API PROJECTS
   //Timestamp Micro Service
-app.use("/timestamp-microservice", require("/API-projects/timestamps.js"));
+app.use("/timestamp-microservice", require("./API-projects/timestamps.js"));
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port', process.env.PORT || 3000);
 })
