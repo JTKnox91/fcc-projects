64687b76dfe83416507a17703c27ed35ffc40315
jtknox91 <jtknox91@gmail.com>
2017-05-15 21:11:17 -0700

    Added route for Timestamps

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// API PROJECTS
+  //Timestamp Micro Service
+app.use("/timestamp-microservice", require("/API-projects/timestamps.js"));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
