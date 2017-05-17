e386f7ab55488ee3e8d10a7eb653b920e77c09a6
jtknox91 <jtknox91@gmail.com>
2017-05-16 22:34:09 -0700

    Added route for to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express()
 // API PROJECTS
   //Timestamp Micro Service
 app.use("/timestamp-microservice", require("./API-projects/timestamps.js"));
+app.use("/request-header-parser", require("./API-projects/req-parser.js"));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port', process.env.PORT || 3000);
